ff396ff8a5698f7b72d3c78b7e239a1d4f3b98b8
Millefeiulle <iamsaaddurrani@gmail.com>
2024-03-09 10:52:45 +0400

    Update Exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -1,11 +1,12 @@
-print ('''
+# print 
+print  (""" 
 Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
 	How I wonder what you are 
-       ''')
+       """)
 
 
       
